511d63883a34f6dc53fb92897b3ac3eabd11560b
Zoey <zoey.spessanha@zeetech.io>
2019-12-07 21:12:58 -0300

    posição certa (incompleta)

--- trabalhos/1posicao_certa.c
+++ trabalhos/1posicao_certa.c
@@ -3,45 +3,53 @@
 #include <time.h>
 
 
-void informa_n (int *n) {
-    printf("Informe o pivô: \n");
-    scanf("%d", n);
+void define_tam_vet (int *tam_vet) {
+    printf("Qual será o tamanho do vetor?\n");
+    scanf("%d", tam_vet);
 }
 
-void gera_vet (int vet[]) {
-    srand(time(NULL));
-    for (int i = 0; i <= 14; i++) {
+void informa_pivo (int *pivo) {
+    printf("Informe o pivô: \n");
+    scanf("%d", pivo);
+}
+
+void gera_vet (int vet[], int tam) {
+    //srand(time(NULL));
+    for (int i = 0; i <= tam; i++) {
         vet[i] = (rand() % 100);
     }
 }
 
-void print_vet (int vet[]) {
+void print_vet (int vet[], int tam) {
     printf("\n");
-    for (int i = 0; i <= 14; i++) {
+    for (int i = 0; i <= tam; i++) {
         printf("%d ", vet[i]);
     }
-    printf("\n");
+    printf("\n\n");
 }
 
-void order_pivot (int vet[], int n) {
+void order_pivot (int vet[], int pivo, int tam_vet) {
     int tmp;
-    
-    for (int i = 0; i <= 14; i++) {
-        if (vet[i]) {
-            
+
+    for (int i = 0; i <= tam_vet; i++) {
+        if (vet[i] > pivo) {
+            tmp = vet[i];
+            vet[i] = vet[i+1];
+            vet[i+1] = tmp;
         }
-        
     }
-    
 }
 
 int main() {
-    int n;
+    int pivo;
     int vet[100];
+    int tam_vet;
 
-    gera_vet(vet);
-    print_vet(vet);
-    informa_n(&n);
-    order_pivot(vet, n);
+    define_tam_vet(&tam_vet);
+    gera_vet(vet, tam_vet);
+    print_vet(vet, tam_vet);
+    informa_pivo(&pivo);
+    order_pivot(vet, pivo, tam_vet);
+    print_vet(vet, tam_vet);
     return 0;
 }
